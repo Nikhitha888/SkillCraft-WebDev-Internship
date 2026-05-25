8faf115d93a71660d21b6040c2bb63308a2aa23c
Nikhitha888 <nikhithavejandla@gmail.com>
2026-05-25 13:50:35 +0530

    Clean repo structure: moved tasks into folders

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-    const navbar= this.document.getElementById("navbar");
-    if(this.window.scrollY > 50){
-        navbar.classList.add("scrolled");
-    }else{
-        navbar.classList.remove("scrolled");
-    }
-});
